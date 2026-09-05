2893cb3317b7c33b523efec2b1b821236f73949b
SunHongJae <sunhongjae@gmail.com>
2026-09-05 22:56:45 +0900

    2026 09 05 finish commit 2

--- ComputerGraphics2/readData.cpp
+++ ComputerGraphics2/readData.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+﻿#include <iostream>
 #include <fstream>
 #include <vector>
 #include <sstream>
@@ -20,7 +20,7 @@ uniform_int_distribution<int> dis{ 1, 6 };
 
 
 class TextData {
-
+			
 private:
 	vector<string> strData;
 	int line;
@@ -63,6 +63,9 @@ public:
 void changeCase(const TextData& textData, bool& switchCase);
 void wordOutput(const TextData& textData);
 void setUpperColor(const TextData& textData, bool& switchUpperColor);
+void reverseOutput(const TextData& textData, bool& switchReverse);
+void changeSpaceToStar(const TextData& textData, bool& switchSpace);
+void reverseWords(const TextData& textData, bool& switchReverseWords);
 
 
 int main() {
@@ -73,6 +76,10 @@ int main() {
 
 	bool switchCase{ true };
 	bool switchUpperColor{ true };
+	bool switchReverse{ true };
+	bool switchSpace{true};
+	bool switchReverseWords{ true };
+
 
 	cout << "input data file name: ";
 	cin >> fileName;
@@ -108,23 +115,23 @@ int main() {
 		}
 		else if (input == 'a') {
 			changeCase(textData, switchCase);
-
 		}
 		else if (input == 'b') {
 			wordOutput(textData);
-
 		}
 		else if (input == 'c') {
 			setUpperColor(textData, switchUpperColor);
 		}
 		else if (input == 'd') {
-
+			reverseOutput(textData, switchReverse);
 		}
 		else if (input == 'e') {
-
+			// e는 원본 수정하는건지? , *을 계속 남기는건지 물어봐야됨.
+			changeSpaceToStar(textData, switchSpace);
 		}
 		else if (input == 'f') {
-
+			// e로 추가된 *를 기준으로 구분하기를 원하는건지 물어보자. 
+			reverseWords(textData, switchReverseWords);
 		}
 		else if (input == 'g') {
 
@@ -188,7 +195,6 @@ void changeCase(const TextData& textData, bool& switchCase)
 }
 // 인자로 const TextData로 받으면 TextData 클래스의 const 멤버 함수만 호출가능하다는 애기다. 
 // 멤버 함수에 const가 붙어있으면 그 함수에서 멤버 변수를 변경하지 않겠다는 약속이다. 
-// vector<string>& 은 원본 수정 가능한 값을 반환하는거라서 const 멤버 함수랑 상충된다. 
 // const vector<string>&을 반환해야됨.
 
 
@@ -212,13 +218,12 @@ void wordOutput(const TextData& textData)
 
 void setUpperColor(const TextData& textData, bool& switchUpperColor)
 {
-	TextData tempData;
-
 	if (switchUpperColor) {
 
-		int count = textData.getLine();
+		int cntColor{};
+		int lineCount = textData.getLine();
 
-		for (int i = 0; i < count; ++i) {
+		for (int i = 0; i < lineCount; ++i) {
 			stringstream lineStream(textData.getStrData()[i]);
 			string temp;
 
@@ -228,13 +233,13 @@ void setUpperColor(const TextData& textData, bool& switchUpperColor)
 				if (isupper(firstChar)) {
 					SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), dis(gen));
 					cout << temp << " ";
+					++cntColor;
 				}
 				else {
 					SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 7);
 					cout << temp << " ";
 				}
 				
-
 			}
 			
 			cout << endl;
@@ -242,11 +247,119 @@ void setUpperColor(const TextData& textData, bool& switchUpperColor)
 		}
 
 		SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 7);
+		cout << endl;
+		cout << "Number of Changed Colors: " << cntColor << endl;
+		
 		switchUpperColor = false;
 	}
 	else {
 		cout << textData;
 		switchUpperColor = true;
+	}
+}
+
+
+
+void reverseOutput(const TextData& textData, bool& switchReverse)
+{
+
+	if (switchReverse) {
+		
+		int lineCount = textData.getLine();
+
+		for (int i = 0; i < lineCount; ++i) {
+			string temp = textData.getStrData()[i];
+			
+			reverse(temp.begin(), temp.end());
+			
+			cout << temp << endl;
+		}
+
+		
+		switchReverse = false;
+	}
+	else {
+		cout << textData;
+		switchReverse = true;
+	}
+
+}
+
+
+void changeSpaceToStar(const TextData& textData, bool& switchSpace)
+{
+	TextData tempData;
+
+	if (switchSpace) {
+
+		int lineCount = textData.getLine();
+
+		for (int i = 0; i < lineCount; ++i) {
+			stringstream lineStream(textData.getStrData()[i]);
+			string temp;
+			string result;
+
+			bool first{ true };
+			while (lineStream >> temp) {
+				
+				if (!first) result += "*";
+				result += temp;
+				first = false;
+			
+			}
+
+			tempData.getStrData().push_back(result);
+		
+		}
+
+		cout << tempData << endl;
+		switchSpace = false;
+	}
+	else {
+		cout << textData;
+		switchSpace = true;
+	}
+
+}
+
+void reverseWords(const TextData& textData, bool& switchReverseWords)
+{
+	if (switchReverseWords) {
+
+		int lineCount = textData.getLine();
+
+		for (int i = 0; i < lineCount; ++i) {
+			string temp = textData.getStrData()[i];
+
+			for (int j = 0; j < temp.size(); ++j) {
+				if (temp[j] == '*')
+					temp[j] = ' ';
+			}
+
+			stringstream tempLine(temp);
+			vector<string> words;
+			string tempWord;
+			bool first{ true };
+			words.reserve(100);
+
+			while (tempLine >> tempWord) {
+				words.push_back(tempWord);
+			}
+
+			for (auto it = words.rbegin(); it != words.rend(); ++it) {
+				if (!first) cout << *it << " ";
+				else cout << *it;
+
+				first = false;
+			}
+
+			cout << endl;
+		}
+		switchReverseWords = false;
+	}
+	else {
+		cout << textData;
+		switchReverseWords = true;
 	}
 
 }
