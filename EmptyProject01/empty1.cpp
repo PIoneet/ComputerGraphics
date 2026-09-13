a86cb0698c1829a386f18bec29dc2903bfe9250d
SunHongJae <sunhongjae@gmail.com>
2026-09-13 10:57:54 +0900

    # 2026 09 12 commit

---
+++ EmptyProject01/empty1.cpp
@@ -0,0 +1,261 @@
+﻿#include <iostream>
+#include <windows.h>
+#include <vector>
+#include <map>
+#include <functional>
+#include <string>
+
+using namespace std;
+
+/*
+1. struct Board 구조체 만들기 
+- 1.1 30 x 30 크기를 가진다. 각 칸은 .를 찍어 표현한다. 
+- 1.2 사용자는 좌표 1개와 가로 길이, 세로 길이를 입력받는다.
+- 1.3 위 데이터를 저장 가능한 struct를 만든다. 
+2. 사용자는 총 2개의 도형을 입력해서 표현한다. 
+- 2.1 도형을 이동, 확대/축소 , 각 좌표축을 이용한 확대 및 축소가 가능하다. 
+- 2.2 도형이 서로 겹치게 되면 그것도 검사해서 다른 색으로 표현해야 한다. 
+3. x/X는 오른쪽/ 왼쪽으로 이동한다. 보드 밖으로 나가면 그 부분만 반대편에 나타나야 한다. 
+- 3.1 y/Y는 아래쪽/ 위쪽으로 이동한다. 
+- 3.2 s/S는 x축 y축 동시에 한 칸 축소/ 확대한다.
+- 3.3 i/I는 x축 한칸 확대/ 축소한다. 
+- 3.4 j/J는 y축 한칸 확대/ 측소한다. 
+- 3.5 a/A는 x축 한칸 확대, y축 한칸 축소/ x축 한칸 축소, y축 한칸 확대한다. 
+- 3.6 b는 사각형의 면적 출력 (가로 x 세로)한다. 
+- 3.7 도형 2개는 다른 명령어를 적용해서 변환해야 한다. 
+4. 입력 가능한 명령어들 
+- 4.1 c: 보드칸을 x축 y축 한칸씩 늘릴 수 있다. 최대 10칸 가능. 
+- 4.2 d: 보드칸을 x축, y축 모두 한칸씩 줄인다. 최대 20칸 줄일 수 있다. 도형을 보드칸 내에 있도록 shift
+- 4.3 r: 모든 걸 리셋하고 다시 좌표값을 입력 받을 수 있어야 한다. 
+- 4.4 q: 프로그램 종료.
+
+*/
+struct Shape {
+
+	int x;
+	int y;
+	int w;
+	int h;
+
+	void moveRowRight() { ++x; }
+	void moveRowLeft() { --x; }
+
+	void moveColDown() { ++y; }
+	void moveColUp() { --y; }
+	
+	void scaleMinus() { --w, --h; }
+	void scalePlus() { ++w, ++h; }
+	
+	void scaleRowPlus() { ++w; }
+	void scaleRowMinus() { --w; }
+	
+	void scaleColPlus() { ++h; }
+	void scaleColMinus() { --h; }
+	
+	void plusRowMinusCol() { ++w, --h; }
+	void plusColMinusRow() { --w, ++h; }
+
+	int dimension() { return w* h; }
+
+};
+
+
+
+
+class Board {
+
+private:
+	vector<vector<char>> grid;
+	// 보드칸은 어쩌피 30x30 칸의 그리드 이기는함.
+	int width{30};
+	int height{30};
+	int expanBoard{ 10 };
+	int cutBoard{ 20 };
+
+public: 
+	
+	Board() {
+		grid.resize(height);
+		for (int i = 0; i < height; ++i) {
+			grid[i].resize(50, '.');
+		}
+			
+	}
+
+	void setUp() {  // 다시 30x30의 grid로 만든다. 
+
+		width = 30;
+		height = 30;
+
+		expanBoard = 10;
+		cutBoard = 20;
+	}
+
+	// s1은 '0' 출력하고, s2는 'X' 출력하고 겹치는 부분은 다른 색깔로 # 출력하면됨.
+	void print(const Shape& s1, const Shape& s2) {
+
+		for (int i = 0; i < height; ++i) {
+
+			for (int j = 0; j < width; ++j) {
+
+				if (s1.x <= j && j <= s1.x + s1.w - 1 &&
+					s1.y <= i && i <= s1.y + s1.h - 1 &&
+					s2.x <= j && j <= s2.x + s2.w - 1 &&
+					s2.y <= i && i <= s2.y + s2.h - 1) {
+
+					SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 4);
+					grid[i][j] = '#';
+					cout << grid[i][j] << " ";
+					SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 7);
+				}
+				else if (s1.x <= j && j <= s1.x + s1.w - 1 &&
+					s1.y <= i && i <= s1.y + s1.h - 1) {
+
+					grid[i][j] = '0';
+					cout << grid[i][j] << " ";
+				}
+				else if (s2.x <= j && j <= s2.x + s2.w - 1 &&
+					s2.y <= i && i <= s2.y + s2.h - 1) {
+
+					grid[i][j] = 'X';
+					cout << grid[i][j] << " ";
+
+				}
+				else {
+					grid[i][j] = '.';
+					cout << grid[i][j] << " ";
+				}
+
+			}
+			cout << endl;
+		}
+
+	}
+
+};
+
+
+void inputShape(Board& board, Shape& s1, Shape& s2)
+{
+	int x, y, w, h;
+	int x2, y2, w2, h2;
+
+	cout << "1번 도형을 입력해주세요: ";
+
+	cin >> x >> y >> w >> h;
+
+	cout << "2번 도형을 입력해주세요: ";
+
+	cin >> x2 >> y2 >> w2 >> h2;
+
+	s1.x = x;
+	s1.y = y;
+	s1.w = w;
+	s1.h = h;
+
+	s2.x = x2;
+	s2.y = y2;
+	s2.w = w2;
+	s2.h = h2;
+
+	board.setUp();
+}
+
+Shape s1;
+Shape s2;
+
+map<string, function<void()>> s1Funcs{
+	{"x", [&] {s1.moveRowRight(); }},
+	{"X", [&] {s1.moveRowLeft(); }},
+	{"y", [&] {s1.moveColDown(); }},
+	{"Y", [&] {s1.moveColUp(); }},
+	{"s", [&] {s1.scaleMinus(); }},
+	{"S", [&] {s1.scalePlus(); }},
+	{"i", [&] {s1.scaleRowPlus(); }},
+	{"I", [&] {s1.scaleRowMinus(); }},
+	{"j", [&] {s1.scaleColPlus(); }},
+	{"J", [&] {s1.scaleColMinus(); }},
+	{"a", [&] {s1.plusRowMinusCol(); }},
+	{"A", [&] {s1.plusColMinusRow(); }},
+
+};
+
+
+map<string, function<void()>> s2Funcs{
+	{"x2", [&] {s1.moveRowRight(); }},
+	{"X2", [&] {s1.moveRowLeft(); }},
+	{"y2", [&] {s1.moveColDown(); }},
+	{"Y2", [&] {s1.moveColUp(); }},
+	{"s2", [&] {s1.scaleMinus(); }},
+	{"S2", [&] {s1.scalePlus(); }},
+	{"i2", [&] {s1.scaleRowPlus(); }},
+	{"I2", [&] {s1.scaleRowMinus(); }},
+	{"j2", [&] {s1.scaleColPlus(); }},
+	{"J2", [&] {s1.scaleColMinus(); }},
+	{"a2", [&] {s1.plusRowMinusCol(); }},
+	{"A2", [&] {s1.plusColMinusRow(); }},
+
+};
+
+
+
+int main()
+{
+	
+	Board board;
+
+	inputShape(board, s1, s2);
+
+	do {
+		string input;
+	
+		
+		cout << "명령을 입력해주세요: ";
+		cin >> input;
+
+		if (input == "q") {
+			cout << "프로그램을 종료합니다. " << endl;
+			return 0;
+		}
+		else if (input == "c") {
+
+		}
+		else if (input == "d") {
+
+		}
+		else if (input == "r") {
+			cout << endl;
+			inputShape(board, s1, s2);
+		}
+		else {
+			// 그냥 일반적으로 변환 명령어 적을떄 모든 경우의 수
+
+			if (s1Funcs.count(input)) {
+				s1Funcs[input]();  // 람다를 호출하라는 거인듯
+			}
+			else if (s2Funcs.count(input)) {
+				s2Funcs[input](); 
+			}
+
+		}
+
+		board.print(s1, s2);
+
+	} while (true);
+
+	
+
+}
+
+
+
+// 람다는 이름 없는 클래스다.
+// auto f1 = [](int a){return a;}
+// auto f2 = [](int a){return a;}
+
+// 둘은 엄연히 서로 다른 클래스다. 
+
+// 이렇게 다른 람다들을 하나의 컨테이너에 묶을 수 있는것이 function<반환형타입(들어가는 인자)> 컨테이너다. 
+
+// auto f3 = [&]{s1.moveX}; 이거는 moveX함수의 코드를 복붙한게 아니라 람다가 호출되면 그때 moveX를 실제로 호출하는 방식이다. 
+// 캡쳐 &은 람다 외부의 변수를 다 참조로 읽을 수 있게 하겠다는 것을 의미함. s1만 읽고 싶으면 s1&을 할 수도 있음. 
--- EmptyProject2/empty2.cpp
+++ EmptyProject2/empty2.cpp
@@ -4,6 +4,42 @@
 #include <fstream>
 #include <sstream>
 #include <cctype>
+#include <algorithm>
+#include <array>
+
+
+/*1. Vertex 구조체 구현
+* 2. TextCoorder 구현
+* 3. 삼각형 face 구조체 구현
+* 4. 파일 읽어서 저장하는 부분 구현
+*  - 4.1 파일 못 읽었을떄 예외처리 구현
+* 
+* 5. 각 getline해서 줄의 앞 string을 보고 if else문 처리하기
+* - 5.1 "f"일뗴 1/1 2/2 3/3을 처리하기
+* - 5.2 iss >> firstWord 공백일떄 continue 처리했음. 
+* 
+* 6. 삼각형 면의 개수만큼 삼각형의 좌표값과 텍스쳐 좌표값을 출력해야 한다. 
+* - 6.1 이 삼각형이 몇번 face 삼각형인지도 미리 저장해야됨. 
+* - 6.2 하나의 면에서 꼭짓점이 3개가 아닌경우.  
+* - 6.3 삼각형의 꼭짓점이 중복된 경우 
+* - 6.4 인덱스 범위 외의 인덱스로 삼각형, 텍스쳐 구성될떄
+* - 6.5 허용되지 않는 문자값 포함되있을떄
+* - 6.6 f의 각 인덱스들을 확인해서 삼각형이 이루어지지 않을떄 에러메시지 출력하기.
+* - 6.7 텍스쳐 좌표 인덱스가 없는 경우도 처리해야 된다. 
+* - 6.8 출력할떄 face 1 (2,3,4) 처럼 몇번째 면의 삼각형이고 정점 좌표 인덱스 출력해야 한다. (1이 시작)
+* 
+
+*/
+
+
+// 주석 작성의 핵심은 내가 어느 길로 나아가야 하는지 알려주는 척도일 뿐이다. 
+// 너무 새새하게 작성할 필요는 없다. 적당히 큼지막하게 적고. 막상 구현해보면서 복잡한 것들은
+//다시 주석으로 돌아서 더 새새하게 과정을 잘라서 작성해주면 된다. 
+//그렇게 하나하나 달성해가는 재미를 느끼면 된다. 
+
+// 뭔가 딱 과제 보면 머릿속에서 설계도가 자동으로 그려져서 한번 슥 보고 막힘없이 코드 쓰는 걸 동경하지도 말고
+// 그걸 자기 자신에게 강요도 하지 마라. 지금은 그렇지 않은게 당연한거고 배워가는 과정임을 명시하자. 
+
 
 using namespace std;
 
@@ -22,24 +58,79 @@ struct TextCoorder {
 
 struct TriangleFace {
 
-	vector<string>& faceLine;
-	int lineNumber{};
+	array<int, 3> faceVertex;
+	array<int, 3> faceCoord;
+	// 이 faceLine에 들어있는 각 줄의 인덱스 정보를 가져와서 출력해야된다.
+	// 그럼 vecotr<TriangleFace> 타입의 자료구조가 있어야 될 것 같은데?
+	int lineFace{};
 
 };
 
 
-void readObjFile( ifstream& in, vector<Vertex>& vertices, vector<TextCoorder>& textCoorders ) {
+int readObjFile(ifstream& in)
+{
+	string fileName;
 
-	string temp;
-	
-	while (getline(in, temp)) {
+	cout << "파일 이름을 입력해주세요 : ";
+	cin >> fileName;
 
-		istringstream iss(temp);
+	in.open(fileName);
+
+	if (!in.is_open()) {
+		return 0;
+	}
+
+	return 1;
+}
+
+
+void parseFace(string word, vector<Vertex>& vertices, vector<TextCoorder>& textCoorders, vector<TriangleFace>& lineTriangle, 
+	array<int, 3>& vIdx, array<int, 3>& vtIdx, int idx, int& lineFaceNum)
+{
+	// 지금 word에는 1/2 뭐 이런 예시값이 들어있음.
+	// 지금은 1, 2가 string값이라서 stoi()해줘서 저장하면 될듯. 
+	// 현재 상황은 v,vt 이렇게 정점 텍스쳐가 다 저장되있는 상황임. 그걸 활용하는 단계이고. 
+
+
+	size_t point = word.find("/");
+
+	vIdx[idx] = stoi( (point == string::npos) ? word : word.substr(0, point) );
+	string temp = (point == string::npos) ? "" : word.substr( point + 1 ) ;
+
+	vtIdx[idx] = -1;  // 예외 체크하고 ""이라면 -1저장. 이후 -1이면 텍스쳐 좌표 출력 안하는 방향으로 가면 될듯. 
+	if (!temp.empty())
+		vtIdx[idx] = stoi(temp);
+
+		
+	if (idx == 2) {
+		++lineFaceNum;
+		lineTriangle.push_back( TriangleFace{ vIdx, vtIdx, lineFaceNum } );
+	}
+
+}
+
+
+void dealWithFile( ifstream& in, vector<Vertex>& vertices, vector<TextCoorder>& textCoorders, vector<TriangleFace>& lineTriangle ) 
+{
+
+	string line;
+	int lineFaceNum{};
+
+	while (getline(in, line )) {  //매줄 처리하기 없으면 탈출
+
+		istringstream iss(line);
 
 		string firstWord;
 
-		iss >> firstWord;
+		array<int, 3> vIdx;
+		array<int, 3> vtIdx;
+		int idx{};
+		
 
+		if (!(iss >> firstWord) ) {
+			continue;
+		}
+		
 		
 		if (firstWord == "#") {
 			continue;
@@ -48,9 +139,10 @@ void readObjFile( ifstream& in, vector<Vertex>& vertices, vector<TextCoorder>& t
 
 			string word;
 			vector<float> tempVertex;
+			tempVertex.reserve(5);
 
 			while (iss >> word) {
-				float val = stoi(word);
+				float val = stof(word);
 
 				tempVertex.push_back(val);
 			}
@@ -64,8 +156,10 @@ void readObjFile( ifstream& in, vector<Vertex>& vertices, vector<TextCoorder>& t
 			string word;
 			vector<float> tempCoord;
 
+			tempCoord.reserve(5);
+
 			while (iss >> word) {
-				float val = stoi(word);
+				float val = stof(word);
 
 				tempCoord.push_back(val);
 			}
@@ -73,42 +167,105 @@ void readObjFile( ifstream& in, vector<Vertex>& vertices, vector<TextCoorder>& t
 			textCoorders.push_back(TextCoorder{ tempCoord[0], tempCoord[1] });
 
 		}
+		else if (firstWord == "f") {
+			// 이 1/2 2/3 3/4를 처리하는 것이 관건이다. 
+			// 이게 일단 정점 버퍼의 1,2,3을 가져오고 텍스쳐 좌표의 2,3,4인덱스를 가져오라는거기는해.
+			// 혹시 만약 1 2 3 이런 구조라면 정점 버퍼 인덱스를 애기하는거고 텍스쳐 좌표 인덱스는 없는 것이다. 
+			string word;
+
+			while (iss >> word) {
+				parseFace(word, vertices, textCoorders, lineTriangle, vIdx, vtIdx, idx, lineFaceNum );
+				++idx;
+			}
+
+		}
 		else {
 			cout << "유효하지 않는 문자열입니다. " << endl;
 
 		}
-	
-
 
 	}
-
-	
-
 }
 
+void printTriangleFace(const vector<Vertex>& vertices, const vector<TextCoorder>& textCoorders, const vector<TriangleFace>& lineTriangle)
+{
+	
+	int size = lineTriangle.size();
+
+
+
+	for (int i = 0; i < size; ++i) {
+		int vIdx[3];
+		
+
+		int vtIdx[3];
+
+		vIdx[0] = lineTriangle[i].faceVertex[0];
+		vIdx[1] = lineTriangle[i].faceVertex[1];
+		vIdx[2] = lineTriangle[i].faceVertex[2];
+	
+		vtIdx[0] = lineTriangle[i].faceCoord[0];
+		vtIdx[1] = lineTriangle[i].faceCoord[1];
+		vtIdx[2] = lineTriangle[i].faceCoord[2];
+		
+		int verticesSize = vertices.size()-1;
+
+		if (vIdx[0] == vIdx[1] || vIdx[1] == vIdx[2] || vIdx[2] == vIdx[0] ) {
+
+			cout << "삼각형 꼭짓점이 중복되고 있습니다. " << endl;
+			return;
+
+		}
+		else if (vIdx[0] - 1 < 0 || vIdx[0] - 1 > verticesSize || vIdx[1] - 1 < 0 || vIdx[1] - 1 > verticesSize || vIdx[2] - 1 < 0 || vIdx[2] - 1 > verticesSize) {
+
+			cout << "정점 버퍼의 인덱스 범위를 넘어섰습니다. " << endl;
+			return;
+		}
+			 
+
+		cout << "Face" << " " << lineTriangle[i].lineFace << " " << "(" << vIdx[0] << ", " << vIdx[1] << ", " << vIdx[2] << "): "
+			<< "vertex " << "(" << vertices[vIdx[0] - 1].x << ", " << vertices[vIdx[0] - 1].y << ", " << vertices[vIdx[0] - 1].z << ") "
+			<< "(" << vertices[vIdx[1] - 1].x << ", " << vertices[vIdx[1] - 1].y << ", " << vertices[vIdx[1] - 1].z << ") "
+			<< "(" << vertices[vIdx[2] - 1].x << ", " << vertices[vIdx[2] - 1].y << ", " << vertices[vIdx[2] - 1].z << ") ";
+
+		cout << endl;
+
+		if (vtIdx[0] != -1 || vtIdx[1] != -1 || vtIdx[2] != -1) {
+
+			cout << "texture " << "(" << textCoorders[vtIdx[0] - 1].u << ", " << textCoorders[vtIdx[0] - 1].v << ") "
+				<< "(" << textCoorders[vtIdx[1] - 1].u << ", " << textCoorders[vtIdx[1] - 1].v << ") "
+				<< "(" << textCoorders[vtIdx[2] - 1].u << ", " << textCoorders[vtIdx[2] - 1].v << ") ";
+
+		}
+
+		cout << endl << endl;
+
+	}
+}
 
 
 int main()
 {
-	string fileName;
+
 	vector<Vertex> vertices;
 	vector<TextCoorder> textCoorders;
+	vector<TriangleFace> lineTriangle;
 
 	vertices.reserve(100);
 	textCoorders.reserve(100);
+	lineTriangle.reserve(100);
 
-	cout << "파일 이름을 입력해주세요 : ";
-	cin >> fileName;
-
-	ifstream in{ fileName };
-
-	if (!in.is_open()) {
-		cout << " 파일을 열 수 없습니다. " << endl;
-		return -1;
-	}
+	ifstream in;
 	
-	readObjFile( in, vertices, textCoorders);
+	while (!readObjFile(in)) {  
+		// 0을 반환하고 뒤집으면 true가 되는듯. 
+		// 그니까 0은 false고 !하면 true가 되는거네. 
+		system("cls");
+	}
 
+	dealWithFile(in, vertices, textCoorders, lineTriangle );   // 여기서 파일 제데로 읽고 자료구조에 저장한다. 
+
+	printTriangleFace(vertices, textCoorders, lineTriangle);
 
 
 }
